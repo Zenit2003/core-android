3f4bdbfd7f4c2b6b7ce6d1ea13e18cd5b8ceb6b1
Zenit2003 <francobusquets@gmail.com>
2021-11-04 19:33:46 +0100

    Hito 2 Completado

--- Proyecto/app/src/main/java/com/example/hitosandroid/MainActivity.java
+++ Proyecto/app/src/main/java/com/example/hitosandroid/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.hitosandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView texto;
+    private Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        texto = findViewById(R.id.texto_hito);
+
+        boton = findViewById(R.id.boton_hito);
+        boton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                texto.setText("Funciona");
+            }
+        });
     }
 }
